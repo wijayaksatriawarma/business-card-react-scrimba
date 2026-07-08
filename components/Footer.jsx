d829317ae1be749618a6c54e58af4c6f9b88c930
Wijaya <agungwarmawijaya@gmail.com>
2026-07-08 14:17:33 +0800

    Adding style to header, main, and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return (
         <footer>
             <img src="../src/assets/square-twitter-brands-solid-full.svg" alt="Twitter" />
-            <img src="../src/assets/square-twitter-brands-solid-full.svg" alt="Twitter" />
-            <img src="../src/assets/square-twitter-brands-solid-full.svg" alt="Twitter" />
-            <img src="../src/assets/square-twitter-brands-solid-full.svg" alt="Twitter" />
-            <img src="../src/assets/square-twitter-brands-solid-full.svg" alt="Twitter" />
+            <img src="../src/assets/square-facebook-brands-solid-full.svg" alt="Facebook" />
+            <img src="../src/assets/square-instagram-brands-solid-full.svg" alt="Instagram" />
+            <img src="../src/assets/square-github-brands-solid-full.svg" alt="GitHub" />
+            <img src="../src/assets/linkedin-brands-solid-full.svg" alt="LinkedIn" />
         </footer>
     )
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,16 @@
 export default function Main() {
     return (
         <main>
-            <section>
+            <section className="top-main">
                 <h1>Pieter John</h1>
-                <p>Frontend Developer</p>
+                <p className="occupation">Frontend Developer</p>
                 <p>pieterjohn.website</p>
-                <div>
-                    <button>Email</button>
-                    <button>LinkedIn</button>
+                <div className="button-container">
+                    <button><img src="../src/assets/envelope-solid-full.svg" alt="envelope-logo" /><span>Email</span></button>
                 </div>
             </section>
 
-            <section>
+            <section className="bottom-main">
                 <h2>About</h2>
                 <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
                 <h2>Interest</h2>
